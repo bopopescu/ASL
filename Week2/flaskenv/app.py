e694de446d17b834c697beab514fc081e48965f7
cmbaseball94 <Chris@Chriss-MacBook-Pro-3.local>
2015-04-14 18:54:03 -0400

    updates
    updates

---
+++ Week2/flaskenv/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
+
+
+app = Flask(__name__)
+
+app.secret_key = "my precious"
+
+@app.route('/')
+def home():
+    # return "Hello, World!"  
+    return render_template('index.html')
+
+@app.route('/welcome')
+def welcome():
+    return render_template("welcome.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid credentials. Please try again.'
+		else:
+			session['logged_in'] = True
+			flash('You were just logged in!')
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error)
+
+@app.route('/register/', methods=('GET', 'POST'))
+def register_page():
+	try:
+		c, conn = connection()
+		return("okay")
+	except Exception as e:
+		return(str(e))
+
+@app.route('/gamescores')
+def gamescores():
+    return render_template("gamescores.html")
+
+
+@app.route('/logout')
+def logout():
+		session.pop('logged_in', None)
+		flash('You were just logged out!')
+		return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
